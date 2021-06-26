a951b4787a2e535d2a39c6a4b26f2f8b8de0d30c
Hana Lipschutz <hanalif619@gmail.com>
2021-06-26 18:53:29 +0300

    media

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -7,9 +7,9 @@ export default {
     <i @click="menuToggle = !menuToggle" class="fas fa-bars"></i>
     <div v-if="menuToggle" class="apps">
     <div class="apps-icons">
-    <router-link to="/" ><i class="fas menu-icon fa-home"></i></router-link>
-    <router-link to="/keep"><i class="fas menu-icon fa-sticky-note"></i></router-link>
-    <router-link to="/mail"><i class="fas menu-icon fa-envelope"></i></router-link>
+    <router-link  to="/" ><i @click="closeMenue" class="fas menu-icon fa-home"></i></router-link>
+    <router-link  to="/keep"><i @click="closeMenue" class="fas menu-icon fa-sticky-note"></i></router-link>
+    <router-link  to="/mail"><i @click="closeMenue" class="fas menu-icon fa-envelope"></i></router-link>
     </div>
     <div class="apps-icons">
     <i class="fas menu-icon unactive fa-cart-arrow-down"></i>
@@ -29,6 +29,11 @@ export default {
     data() {
         return {
             menuToggle: false
+        }
+    },
+    methods:{
+        closeMenue(){
+            this.menuToggle = false;
         }
     }
 }
