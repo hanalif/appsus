012cdf07952cf2a397608fc602afa00eb195940e
Hana Lipschutz <hanalif619@gmail.com>
2021-06-26 16:54:26 +0300

    delete close edit btn

--- js/apps/keep/cmps/keep-items-edit/note-todos-edit.js
+++ js/apps/keep/cmps/keep-items-edit/note-todos-edit.js
@@ -5,7 +5,6 @@ export default {
         <input class="note-edit-input" v-model="listTitle" type="text" :placeholder="data.info.title">
         <div class="note-edit-btns-container">
             <button class="keep-app-btn" @click="saveChanges"><i class="fas fa-check"></i></button>
-            <button class="keep-app-btn" @click="closeEditor"><i class="fas fa-times"></i></button>
         </div>
     </section>
     `,
@@ -25,9 +24,6 @@ export default {
             const savedData = { title: this.listTitle, todos: this.todos }
             this.$emit('saveChanges', savedData);
         },
-        closeEditor(){
-            this.$emit('closeEditor');
-        }
     },
     
 }
