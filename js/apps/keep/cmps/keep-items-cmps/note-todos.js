bcf1c0c4ae298d9fb0e3356c5843f04859200ea6
Hana Lipschutz <hanalif619@gmail.com>
2021-06-26 10:33:16 +0300

    style todo note

--- js/apps/keep/cmps/keep-items-cmps/note-todos.js
+++ js/apps/keep/cmps/keep-items-cmps/note-todos.js
@@ -1,17 +1,18 @@
 export default {
     props:['data'],
     template: `
-    <section class="note-todos note-top-margin">
-    <h2>{{data.info.title}}</h2>
-    <input class="add-new-todo-input-field" v-model="addedNewTodo" type="text">
-    <button @click="addTodo">+</button>
-    <ul class="note-todos-list">
-        <li v-for="(todo, index) in data.info.todos" :key="index" class="todo-item"> 
-            <h4 @click="markedTodo(index)" :class=" {'marked-todo': todo.isMarked}">{{todo.txt}}</h4>
-            <button @click="deleteTodo(index)"><i class="fas fa-times"></i></button>
-        </li>
-    </ul>
-
+    <section class="note-todos flex column note-top-margin">
+        <h2>{{data.info.title}}</h2>
+        <div class="add-new-note-container flex">
+            <input class="add-new-todo-input-field" v-model="addedNewTodo" type="text">
+            <button class="keep-app-btn" @click="addTodo"><i class="fas fa-check"></i></button>
+        </div>   
+        <ul class="note-todos-list flex column">
+            <li class="todo-item flex" v-for="(todo, index) in data.info.todos" :key="index"> 
+                <h4 class="note-todo-list" @click="markedTodo(index)" :class=" {'marked-todo': todo.isMarked}">{{todo.txt}}</h4>
+                <button class="keep-app-btn" @click="deleteTodo(index)"><i class="fas fa-times"></i></button>
+            </li>
+        </ul>
     </section>
     `,
     data(){
@@ -26,6 +27,7 @@ export default {
             todos.splice(index, 1);
         },
         addTodo(){
+            if(this.addedNewTodo === '')return
             let todos = this.data.info.todos;
             let newTodo = {txt: this.addedNewTodo, isMarked:false, doneAt: null}
             todos.push(newTodo);
