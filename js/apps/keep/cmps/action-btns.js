20f693f37c02914e5c730deac4f612c4bc943812
Hana Lipschutz <hanalif619@gmail.com>
2021-06-26 22:31:01 +0300

    fix send to mail

--- js/apps/keep/cmps/action-btns.js
+++ js/apps/keep/cmps/action-btns.js
@@ -5,7 +5,7 @@ export default {
         <button class="keep-app-btn" @click="deleteNote"><i class="far fa-trash-alt"></i></button>
         <button class="keep-app-btn" @click="editNote"><i class="far fa-edit"></i></button>
         <button class="keep-app-btn" @click="opencolorPalette"><i class="fas fa-palette"></i></button>
-        <button class="keep-app-btn" @click="onSendMail"><i class="fas fa-envelope"></i></button>
+        <button class="keep-app-btn" title="send mail" @click="onSendMail"><i class="fas fa-envelope"></i></button>
     </section>
     `,
     methods: {
@@ -19,7 +19,22 @@ export default {
             this.$emit('opencolorPalette', this.data.id)
         },
         onSendMail() {
-            let params = { subject: this.data.info.title, body: this.data.info.freeTxt };
+
+            let params;
+            if (this.data.type === 'noteTxt') {
+                params = { subject: this.data.info.title, body: this.data.info.freeTxt };
+            }
+            else if (this.data.type === 'noteImg') {
+                params = { subject: this.data.info.title, body: this.data.info.url };
+            }
+            else if (this.data.type === 'noteVideo') {
+                params = { subject: this.data.info.title, body: this.data.info.url };
+            }
+            else if (this.data.type === 'noteTodos') {
+                params = { subject: this.data.info.title, body: this.data.info.todos.map(t => t.txt).join(',') };
+            }
+            console.log(this.data);
+            console.log(params);
             this.$router.push({ name: 'route.mail', query: params });
         }
     }
